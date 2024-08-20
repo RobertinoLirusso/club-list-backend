caf1af35a62af8bed89455ddf303eec86b930d88
RobertinoLirusso <robertinolirusso3@gmail.com>
2024-08-19 23:10:57 -0300

    db changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,19 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ClubModule } from './club/club.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT, 10),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      autoLoadEntities: true,
-      synchronize: true,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async () => ({
+        type: 'postgres',
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT, 10),
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        autoLoadEntities: true, // Automatización de la carga de entidades
+        synchronize: true, // Para modo de desarrollo (es decir crear las tablas al iniciar iniciar el servidor)
     }),
+  }),
     ClubModule],
   controllers: [AppController],
   providers: [AppService],
