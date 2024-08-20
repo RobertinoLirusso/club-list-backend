77727b16bae70a128c756dbb0fe51af00646f178
RobertinoLirusso <robertinolirusso3@gmail.com>
2024-08-19 23:23:20 -0300

    db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,14 +12,10 @@ import { ConfigModule } from '@nestjs/config';
       imports: [ConfigModule],
       useFactory: async () => ({
         type: 'postgres',
-        host: process.env.DB_HOST,
-        port: parseInt(process.env.DB_PORT, 10),
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        autoLoadEntities: true, // Automatización de la carga de entidades
-        synchronize: true, // Para modo de desarrollo (es decir crear las tablas al iniciar iniciar el servidor)
-    }),
+        url: process.env.DATABASE_URL, 
+        autoLoadEntities: true,
+        synchronize: false,
+      }),
   }),
     ClubModule],
   controllers: [AppController],
