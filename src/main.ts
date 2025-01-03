4b7217a6d00e635563b0149b5b2bdf2fe343fadd
RobertinoLirusso <robertinolirusso3@gmail.com>
2025-01-03 20:42:16 -0300

    cors new change

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['https://club-list-backend.onrender.com/club'],
+    origin: ['https://worldclubs.vercel.app/'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
   await app.listen(3000);
