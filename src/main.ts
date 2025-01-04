7f37eb04da3826c6a79577c2ea5bf9a2ee35b137
RobertinoLirusso <robertinolirusso3@gmail.com>
2025-01-04 18:35:06 -0300

    main new config

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ['https://worldclubs.vercel.app/'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  });
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
